b9c449ae861b77111cf3560941264d552ba3dda0
Leon Gersen <leongersen@gmail.com>
2014-05-29 11:33:57 +0200

    Made JSlint happy

--- wNumb.js
+++ wNumb.js
@@ -281,7 +281,7 @@ var
 		var i, args = [];
 
 		// Add all options in order of FormatOptions
-		for ( i = 0; i < FormatOptions.length; i++ ) {
+		for ( i = 0; i < FormatOptions.length; i+=1 ) {
 			args.push(options[FormatOptions[i]]);
 		}
 
